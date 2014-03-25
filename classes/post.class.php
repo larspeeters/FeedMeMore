56e10f666fde4a822b5d665abc705e7987a8379e
Cedric0712 <cedricbloem@hotmail.com>
2014-03-25 13:35:56 +0100

    bla

--- classes/post.class.php
+++ classes/post.class.php
@@ -56,7 +56,7 @@
 			$mysql_password = "Imd123)";
 			$mysql_database = "a8154344_imd";
 
-			$conn  = new mysqli("mysql_host","mysql_user","mysql_password","mysql_database");
+			$conn  = new mysqli($mysql_host, $mysql_user, $mysql_password, $mysql_database);
 			$sql = "insert into tblPost (subject, mention, text) values ('".$conn->real_escape_string($this->m_sSubject)."', 
 																	  '".$conn->real_escape_string($this->m_sMention)."', 
 																	  '".$conn->real_escape_string($this->m_sText)."');";

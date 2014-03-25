4e8b436af432c603147746dc39e116f85c353ef1
magicalyuki <ellenvdb44@gmail.com>
2014-03-25 12:55:22 +0100

    Inhoud gewijzigd

--- about.php
+++ about.php
@@ -13,6 +13,39 @@ include_once "includes/nav.include.php";
 ?>
    
 	<article>
+    <h1>Wie zijn wij</h1>
+    <h2>Doel</h2>
+    <p>Het doel van de studentenraad kan het beste uitgelegd worden aan de hand van de missie en de visie van SRTMM.
+    </p>
+    <br>
+    <h2>Missie</h2>
+    <p>De Missie verteld waar we als studentenraad voor staan. Het moet onderscheidend zijn, motiverend, specifiek maar toch nog kort en krachtig.</p>
+<br>
+<ul>
+<li>Aanspreekpunt voor studenten</li>
+<li>Belangenvertegenwoordiger van studenten in de hogere organen</li>
+<li>Dynamische instelling</li>
+<li>Begeleider in onderwijsaangelegenheden</li>
+<li>Lage drempel voor studenten naar hogere raden</li>
+<li>Probleemoplossende en probleemvermijdende werking</li>
+</ul>
+<br>
+
+    <h2>Visie</h2>
+    <p>De Visie is wat we willen bereiken binnen een bepaald tijdskader en hoe we het willen bereiken.</p>
+<br>
+<ul>
+<li>Meer naambekendheid</li>
+<li>Meer vertrouwen</li>
+<li>Meer inspraakrecht in de verschillende organen</li>
+<li>Verbeterde relaties met interne & externe organen</li>
+<li>Verbeterde communicatie met interne & externe organen en naar studenten</li>
+<li>Verduidelijking van de hiërarchie</li>
+</ul>
+<br>
+
+  
+    <p></p>
 	</article>
 </body>
 </html>

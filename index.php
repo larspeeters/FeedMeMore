c07c45d0bdaa2bdfb3892b7d20c54a9b909a682b
Cedric0712 <cedricbloem@hotmail.com>
2014-04-01 09:37:41 +0200

    fout door koppeling weggehaald

--- index.php
+++ index.php
@@ -5,11 +5,8 @@
 	<title>Home | Feed Me More</title>
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/screen.css">
-<<<<<<< HEAD
 	<link rel="icon" type="image/png" href="images/favicon.ico">
-=======
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
->>>>>>> b67f4bafd1c7e3c1a9be596b3b4bb1e886ed7233
+	<!--<link rel="icon" href="images/favicon.ico" type="image/x-icon" />-->
 </head>
 
 <body>

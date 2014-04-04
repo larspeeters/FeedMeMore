1e302dd1678cfca696511b03422cc4fa0a3fc52f
Cedric0712 <cedricbloem@hotmail.com>
2014-04-04 12:37:58 +0200

    details pagina toegevoegd

---
+++ details.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<title>Home | Feed Me More</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/screen.css">
+	<link rel="icon" type="image/png" href="images/favicon.ico">
+	<!--<link rel="icon" href="images/favicon.ico" type="image/x-icon" />-->
+</head>
+
+<body>
+<?php
+include_once "includes/nav.include.php";
+?>
+   
+	<article>
+	</article>
+</body>
+</html>

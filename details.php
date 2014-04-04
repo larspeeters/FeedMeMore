5c52518ceb17179f5328c92b6dc0bacb359e8b85
Cedric0712 <cedricbloem@hotmail.com>
2014-04-04 12:40:37 +0200

    details toegevoegd

--- details.php
+++ details.php
@@ -1,4 +1,6 @@
-<!DOCTYPE HTML>
+<?php
+	include('classes/post.class.php');
+?><!DOCTYPE HTML>
 <html lang="nl">
 <head>
 	<meta charset="utf-8">
@@ -15,6 +17,34 @@ include_once "includes/nav.include.php";
 ?>
    
 	<article>
+		<!-- HIER WORD NOG AAN GEWERKT
+	<?php 
+    $p = new Post();
+    $n = $p->show();
+    if($n){
+      foreach($n as $list){
+        echo "<div id='list'><div class='topPost'><h2>".$list['subject']. " | </h2><h3>" .$list['mention']. "</h3></div><p>" .$list['text']."</p></div>";
+        echo "<div class='movie_choice'>
+                  <div id='".$list['id']."' class='rate_widget'>
+                      <div class='star_1 ratings_stars'></div>
+                      <div class='star_2 ratings_stars'></div>
+                      <div class='star_3 ratings_stars'></div>
+                      <div class='star_4 ratings_stars'></div>
+                      <div class='star_5 ratings_stars'></div>
+                      <div id='votes'>
+                        <div class='total_votes'>vote data</div>
+                      </div>
+                  </div>
+              </div><hr>";
+      }
+      if (!empty($_POST['remove']))
+      {
+        $r = new Post();
+        $r->remove();
+      }
+    }
+
+?>-->
 	</article>
 </body>
 </html>

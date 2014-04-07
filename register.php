2c4b21ec48943be22cd1e08be7f1e3e711fb3cad
larspeeters <peeters.lars@telenet.be>
2014-04-07 02:26:10 +0200

    added image upload

--- register.php
+++ register.php
@@ -5,34 +5,27 @@
 	<title>Registreren | Feed Me More</title>
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/screen.css">
-    <link rel="icon" type="image/png" href="images/favicon.ico">
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 </head>
 
 <body>
-<?php 
+<?php include_once "includes/nav.include.php";
 
-// filename: upload.form.php 
+function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+    $url = 'http://www.gravatar.com/avatar/';
+    $url .= md5( strtolower( trim( $email ) ) );
+    $url .= "?s=$s&d=$d&r=$r";
+    if ( $img ) {
+        $url = '<img src="' . $url . '"';
+        foreach ( $atts as $key => $val )
+            $url .= ' ' . $key . '="' . $val . '"';
+        $url .= ' />';
+    }
+    return $url;
+}?>
 
-// first let's set some variables 
-
-// make a note of the current working directory relative to root. 
-$directory_self = str_replace(basename($_SERVER['PHP_SELF']), '', $_SERVER['PHP_SELF']); 
-
-// make a note of the location of the upload handler script 
-$uploadHandler = 'http://' . $_SERVER['HTTP_HOST'] . $directory_self . '/images/'.$_POST['avatar']; 
-
-// set a max file size for the html upload form 
-$max_file_size = 30000; // size in bytes 
-
-// now echo the html page 
-if(!empty($_POST['avatar'])){
-@move_uploaded_file($_POST['avatar'], $uploadHandler);
-}
-include_once "includes/nav.include.php";
-?>
 <div id="container"> 
-	<form action="" method="POST" id="register">
+	<form action="" method="POST" enctype="multipart/form-data" id="register">
     	<fieldset>
         	<legend>Nieuwe gebruiker registeren</legend>
             <label for="firstname">Voornaam: </label> <input type="text" size="50" name="firstname" /><br />
@@ -40,10 +33,13 @@ include_once "includes/nav.include.php";
             <label for="password">Wachtwoord: </label> <input type="password" size="50" name="password" id="password" /><br />
             <label for="passwordRep">Herhaal wachtwoord: </label> <input type="password" size="50" name="passwordRep" id="passwordRep" /><span id="passwordmatch" style="color:red;"> </span><br />
             <label for="email">Email: </label> <input type="text" size="8" maxlength="8" name="email" id="mail"/>@student.thomasmore.be<br />
-            <label for="avatar" >Avatar: </label><img src="images/avatar.png" alt="Your avatar" title="Kies een avatar" width="75" height="75" /> <br /><input type="file" name="avatar" /><br />
+            <label for="avatar" >Avatar: </label><img src="images/avatar.png" alt="Your avatar" title="Kies een avatar" width="75" height="75" id="avatar"/> <br />
+            <input type="file" name="avatar" id="file" /><br />
+            <input type="text" name="gravatar" id="gravatar" /><input type="button" id="image" value="Get gravatar" /><br />
             <input type="submit" value="Registreren" id="btnRegister" />
         </fieldset>
     </form>
+     
 </div>
 </body>
 <script>
@@ -54,6 +50,30 @@ $(document).ready(function(e) {
 		}
 		else{$("#passwordmatch").html("");}
     });
+	$("#file").change(function(){
+		if (this.files && this.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("#avatar").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(this.files[0]);
+    }
+	});
+	
+	$('#image').click(
+    function(){
+       $.ajax({
+          url: "images.php",
+          type: "POST",
+		  data: {"gravatar": $("#gravatar").val()},
+          success: function(data){
+              $("#avatar").attr("src", data);
+          }
+       });
+
+    }
+)
 });
 
 </script>

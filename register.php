4aee45bc81200fdadc19f567d2f8372cdb014719
larspeeters <peeters.lars@telenet.be>
2014-04-08 14:11:07 +0200

    gravatar script

--- register.php
+++ register.php
@@ -10,19 +10,18 @@
 
 <body>
 <?php include_once "includes/nav.include.php";
+		include_once "classes/user.class.php";
 
-function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
-    $url = 'http://www.gravatar.com/avatar/';
-    $url .= md5( strtolower( trim( $email ) ) );
-    $url .= "?s=$s&d=$d&r=$r";
-    if ( $img ) {
-        $url = '<img src="' . $url . '"';
-        foreach ( $atts as $key => $val )
-            $url .= ' ' . $key . '="' . $val . '"';
-        $url .= ' />';
-    }
-    return $url;
-}?>
+		if(!empty($_POST)){
+			$usr = new User();
+			$usr->Firstname = $_POST['firstname'];
+			$usr->Lastname = $_POST['lastname'];
+			$usr->Password = $_POST['password'];
+			$usr->Email = $_POST['email']."@student.thomasmore.be";
+			
+			$usr->Save();
+		}
+?>
 
 <div id="container"> 
 	<form action="" method="POST" enctype="multipart/form-data" id="register">
@@ -64,7 +63,7 @@ $(document).ready(function(e) {
 	$('#image').click(
     function(){
        $.ajax({
-          url: "images.php",
+          url: "scripts/gravatar.php",
           type: "POST",
 		  data: {"gravatar": $("#gravatar").val()},
           success: function(data){

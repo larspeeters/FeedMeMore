00025c76f97c07f1ad6c77b61dd9bc16ebc7d957
Cedric0712 <cedricbloem@hotmail.com>
2014-04-25 12:07:32 +0200

    details gewijzigd

--- details.php
+++ details.php
@@ -21,30 +21,8 @@ include_once "includes/nav.include.php";
 	<?php 
     $p = new Post();
     $n = $p->show();
-    if($n){
-      foreach($n as $list){
-        echo "<div id='list'><div class='topPost'><h2>".$list['subject']. " | </h2><h3>" .$list['mention']. "</h3></div><p>" .$list['text']."</p></div>";
-        echo "<div class='movie_choice'>
-                  <div id='".$list['id']."' class='rate_widget'>
-                      <div class='star_1 ratings_stars'></div>
-                      <div class='star_2 ratings_stars'></div>
-                      <div class='star_3 ratings_stars'></div>
-                      <div class='star_4 ratings_stars'></div>
-                      <div class='star_5 ratings_stars'></div>
-                      <div id='votes'>
-                        <div class='total_votes'>vote data</div>
-                      </div>
-                  </div>
-              </div><hr>";
-      }
-      if (!empty($_POST['remove']))
-      {
-        $r = new Post();
-        $r->remove();
-      }
-    }
-
-?>-->
+    echo "<div id='detailPost'><h2>".$list['subject']."</h2><h3>".$list['mention']."</h3><p>".$list['text']."</p></div>"
+?>
 	</article>
 </body>
 </html>

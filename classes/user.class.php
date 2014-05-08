49419a4658bd83cdf3c5a46659f3d92db05468d2
larspeeters <peeters.lars@telenet.be>
2014-05-08 21:23:59 +0200

    Email support

--- classes/user.class.php
+++ classes/user.class.php
@@ -68,15 +68,16 @@
 				if(substr($this->m_sAvatar,0,7) != "http://" && !empty($this->m_sAvatar))
 					$this->uploadImage();
 				$db = new Database();
-			
-				$sql = "insert into tblUsers (first_name, last_name, password, email, avatar, isAdmin) values ('".$db->conn->real_escape_string($this->m_sFirstname)."', 
+				$hashed = md5( rand(0,1000) );
+				$sql = "insert into tblUsers (first_name, last_name, password, email, avatar, isAdmin, hash) values ('".$db->conn->real_escape_string($this->m_sFirstname)."', 
 																		  '".$db->conn->real_escape_string($this->m_sLastname)."', 
 																		  '".$db->conn->real_escape_string($this->hashed($this->m_sPassword))."',
 																		  '".$db->conn->real_escape_string($this->m_sEmail)."',
 																		  '".$db->conn->real_escape_string($this->m_sAvatar)."',
-																		  0);";
+																		  0,'".$db->conn->real_escape_string($hashed)."');";
 			$db->conn->query($sql);
 			mysqli_close($db->conn); //close connection with Dbase
+			$this->activateAccount($this->m_sEmail, $hashed);
 			}catch(Exception $e){
 				echo $e->getMessage();
 			}
@@ -110,6 +111,26 @@
 			}
 			mysqli_close($db->conn); //close connection with Dbase
 		}
+	private function activateAccount($email, $hash){
+		$to      = $this->m_sEmail; 
+		$subject = 'Account activatie'; 
+		$message = '
+		 
+		Bedankt voor het registreren!
+		Om gebruik te kunnen maken van uw account, dient uw deze te activeren via onderstaande link:
+		http://www.yourwebsite.com/activate.php?email='.$email.'&hash='.$hash.'
+		
+		Uw inloggegevens
+		------------------------
+		Gebruikersnaam: '.$this->m_sEmail.'
+		Wachtwoord: '.$this->m_sPassword.'
+		------------------------
+
+		'; // Our message above including the link
+							 
+		$headers = 'From: Feed Me More' . "\r\n"; // Set from headers
+		mail($to, $subject, $message, $headers); // Send our email	
+	}
 	private function uploadImage(){
 		// In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
 		// of $_FILES.

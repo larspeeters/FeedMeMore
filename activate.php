0aa81390dd1d4c5629e5a9310161e646a1743b3f
larspeeters <peeters.lars@telenet.be>
2014-05-09 01:53:10 +0200

    activation script updated

--- activate.php
+++ activate.php
@@ -1,7 +1,22 @@
 <?php
-	include ("../classes/user.class.php");
+	include ("classes/database.class.php");
 	if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
-    // Verify data
+		$db = new Database();	
+		$sql = "SELECT email, hash, activated FROM tblUsers WHERE email='"
+				.$db->conn->real_escape_string($_GET['email'])."' AND hash='"
+				.$db->conn->real_escape_string($_GET['hash'])."' AND activated='0'";
+
+		if($result = $db->conn->query($sql)){
+			$array = $result->num_rows;
+			if($array == 1){
+				$db = new Database();	
+				$sql = "UPDATE tblUsers SET activated='1' WHERE email='"
+						.$db->conn->real_escape_string($_GET['email'])."' AND hash='"
+						.$db->conn->real_escape_string($_GET['hash'])."' AND activated='0'";
+				if($db->conn->query($sql))
+					echo "Activated!";
+			}
+		}
 	}else{
 		// Invalid approach
 	}

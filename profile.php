fce8d8e0de56bf8b9f17725433d5662fa44ae16e
larspeeters <peeters.lars@telenet.be>
2014-05-08 02:17:49 +0200

    changed profile

---
+++ profile.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<title>Mijn profiel | Feed Me More</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/screen.css">
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+
+<body>
+<?php
+include_once "includes/nav.include.php";
+if(isset($_SESSION)):
+	if($_SESSION["admin"])
+		echo "<span>[Administrator]</span>" ?>
+	<h2>Profiel van <?php echo $_SESSION['username']; 	if(substr($_SESSION['avatar'],0,7) != "http://"){?></h2>
+    <img src="<?php echo "../images/avatars/".$_SESSION['avatar']; ?>" width="100px" height="100px" title="Profielfoto" /><?php } else{ ?> 
+    <img src="<?php echo $_SESSION['avatar']; ?>" width="100px" height="100px" title="Profielfoto" /><?php } ?>
+    <ul>
+    	<li>Gebruikersnaam: <?php echo $_SESSION['username'];?></li>
+        <li>Email: <?php echo $_SESSION['email'];?></li>
+		<li><span id="password">Wachtwoord wijzigen</span>
+        <div id="frmPassword" >
+        	<form action=# method= >
+            <label for="oudWachtwoord">Huidig wachtwoord: </label>
+            <input type="text" size="25" name="oldPass" /><br />
+            <label for="nieuwWachtwoord">Nieuw wachtwoord: </label>
+            <input type="text" size="25" name="newPass" /><br />
+            <label for="herhaalWachtwoord">Herhaal wachtwoord: </label>
+            <input type="text" size="25" name="repPass" />
+            </form>
+            </div>
+        </li>
+    </ul>
+<?php else: header("Location: error.php"); endif;?>
+</body>
+<script>
+$('#password').click(function () {
+        $('#frmpassword').slideDown();
+    });
+</script>
+</html>

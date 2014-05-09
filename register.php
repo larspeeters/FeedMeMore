37f3d031c03ee32acf29da7a0d64e62942ba8401
larspeeters <peeters.lars@telenet.be>
2014-05-09 02:12:58 +0200

    favicon added

--- register.php
+++ register.php
@@ -5,6 +5,7 @@
 	<title>Registreren | Feed Me More</title>
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/screen.css">
+    <link rel="icon" type="images/png" href="images/favicon.ico">
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 </head>
 

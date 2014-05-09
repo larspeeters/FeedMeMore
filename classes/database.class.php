a51e33c922c9db38e424e44a7378370590058428
larspeeters <peeters.lars@telenet.be>
2014-05-09 13:30:38 +0200

    database

--- classes/database.class.php
+++ classes/database.class.php
@@ -1,10 +1,10 @@
 <?php
 	class Database
 	{
-		private $m_sHost =  "localhost";
-		private $m_sUser = "root";
-		private $m_sPassword = "";
-		private $m_sDatabase = "imd";
+		private $m_sHost =  "mysql1.000webhost.com";
+		private $m_sUser = "a8154344_imd";
+		private $m_sPassword = "a8154344_php";
+		private $m_sDatabase = "Imd123)";
 		
 		public $conn;
 

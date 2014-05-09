b6237570ef8c4f04fb10f94aebce695869bfe992
larspeeters <peeters.lars@telenet.be>
2014-05-09 14:15:23 +0200

    sessions added

--- activate.php
+++ activate.php
@@ -1,11 +1,10 @@
 <?php
-	include ("classes/database.class.php");
 	if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+		include ("classes/database.class.php");
 		$db = new Database();	
 		$sql = "SELECT email, hash, activated FROM tblUsers WHERE email='"
 				.$db->conn->real_escape_string($_GET['email'])."' AND hash='"
 				.$db->conn->real_escape_string($_GET['hash'])."' AND activated='0'";
-
 		if($result = $db->conn->query($sql)){
 			$array = $result->num_rows;
 			if($array == 1){
@@ -13,8 +12,19 @@
 				$sql = "UPDATE tblUsers SET activated='1' WHERE email='"
 						.$db->conn->real_escape_string($_GET['email'])."' AND hash='"
 						.$db->conn->real_escape_string($_GET['hash'])."' AND activated='0'";
-				if($db->conn->query($sql))
+				if($db->conn->query($sql)){
+					include ("classes/user.class.php");
+					$usr = new User();
+					$usr->Email = $_GET['email'];
+					$u = $usr->getUser();
+					session_start();
+					$_SESSION['id'] = $u['Id'];
+					$_SESSION['username'] = $u['first_name']." ".$u['last_name'];
+					$_SESSION['email'] = $u['email'];
+					$_SESSION['avatar'] = $u['avatar'];
+					$_SESSION['admin'] = $u['isAdmin'];
 					header("Location: index.php");
+				}
 			}
 		}
 	}else{

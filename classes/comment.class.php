de6007af2a22e94aae33656b6199f87d31e6eaeb
larspeeters <peeters.lars@telenet.be>
2014-05-11 19:37:40 +0200

    Comment count

--- classes/comment.class.php
+++ classes/comment.class.php
@@ -4,6 +4,9 @@ include_once("database.class.php");
 	class Comment
 	{
 		private $m_sComment;
+		private $m_sSubject;
+		private $m_sMention;
+		private $m_sUsername;
 
 		public function __set($p_sProperty, $p_vValue)
 		{
@@ -15,6 +18,15 @@ include_once("database.class.php");
 				}
 					$this->m_sComment = $p_vValue;
 					break;
+				case 'Subject':
+					$this->m_sSubject = $p_vValue;
+					break;
+				case 'Mention':
+					$this->m_sMention = $p_vValue;
+					break;
+				case 'User':
+					$this->m_sUsername = $p_vValue;
+					break;
 			}
 		}
 
@@ -24,27 +36,34 @@ include_once("database.class.php");
 				case 'Comment':
 					return $this->m_sComment;
 					break;
+				case 'Mention':
+					return $this->m_sMention;
+					break;
+				case 'Subject':
+					return $this->m_sSubject;
+					break;
+				case 'User':
+					return $this->m_sUsername;
+					break;
 			}
 		}
 
 		public function AddComment()
 		{
-			session_start();
 			$db = new Database();
-			//$_SESSION['username'] = $u['first_name']." ".$u['last_name'];
-			list($firstname, $lastname) = split(' ', $_SESSION['username']);
+			list($firstname, $lastname) = explode(' ', $this->m_sUsername);
 			
 			$addComm = "insert into tblComments (comment, firstName, lastName, postSubject, postMention) 
-						values ('".$db->conn->real_escape_string($this->m_sComment)."', '".$firstname."', '".$lastname."', '".$_SESSION['subject']."', '".$_SESSION['mention']."');";
+						values ('".$db->conn->real_escape_string($this->m_sComment)."', '".$db->conn->real_escape_string($firstname)
+						."', '".$db->conn->real_escape_string($lastname)."', '".$db->conn->real_escape_string($this->m_sSubject)."', '".$db->conn->real_escape_string($this->m_sMention)."');";
 			$db->conn->query($addComm);
 		}
 
 		public function ShowComments()
 		{
-			session_start();
 			$db = new Database();
 
-			$getComm = "select * from tblComments where postSubject= '".$_SESSION['subject']."' and postMention= '".$_SESSION['mention']."';";
+			$getComm = "select * from tblComments where postSubject= '".$db->conn->real_escape_string($this->m_sSubject)."' and postMention= '".$db->conn->real_escape_string($this->m_sMention)."';";
 			$getCommResult = $db->conn->query($getComm);
 			$arrayComment = array();
 			

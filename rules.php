e977668a5cc3054d406906d3476e79604c2a1013
Cedric0712 <cedricbloem@hotmail.com>
2014-05-11 13:33:28 +0200

    Enkele regels toegevoegd

--- rules.php
+++ rules.php
@@ -5,7 +5,6 @@
 	<title>Regels | Feed Me More</title>
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/screen.css">
-    <link rel="icon" type="image/png" href="images/favicon.ico">
 </head>
 
 <body>
@@ -21,13 +20,40 @@ include_once "includes/nav.include.php";
     </p>
     <br>
     <h3>R1. Blijf beleefd</h3>
-    <p></p>
-
+    <p>Wanneer je een post plaatst, vragen wij dat je zo beleefd mogelijk blijft, ondanks de situatie. Wij zijn er om te luisteren naar de problemen die 
+    van jou en je mede-studenten, maar posts die onbeleefd zijn, negeren wij. Gelieve dus niet te vloeken of docenten en studenten zwart te maken en
+    te beledigen. We zijn en blijven respectvol tegen elkaar! </p>
+    <br>
     <h3>R2. Wees niet herhalend</h3>
+    <p>Wij zullen altijd antwoorden op jouw post, en onze persoonlijke mening geven, maar indien dit nog niet gebeurd is, hoef je geen nieuwe post te sturen.
+    We zullen antwoorden zodra wij de kans hebben, en houden de posts regelmatig in het oog. Ook wanneer jouw post eerder geweigerd werd, of als andere
+    studenten negatief antwoorden, is het niet nodig om de post opnieuw te sturen, tenzij dit door ons vermeld wordt.</p>
+    <br>
+    <h3>R3. Verklaar jezelf duidelijk</h3>
+    <p>Leg ons zo duidelijk mogelijk uit wat jouw probleem is en wat het allemaal inhoud. Als wij er iets aan willen doen, dan moeten we duidelijk
+    geïnformeerd zijn. Verklaar dus duidelijk wat je bedoeld.</p>
+    <br>
+    <h3>R4.</h3>
+    <p></p>
+    <br>
+    <h3>R5.</h3>
+    <p></p>
+    <br>
+    <h3>R6.</h3>
+    <p></p>
+    <br>
+    <h3>R7.</h3>
+    <p></p>
+    <br>
+    <h3>R8.</h3>
+    <p></p>
+    <br>
+    <h3>R9.</h3>
+    <p></p>
+    <br>
+    <h3>R10.</h3>
     <p></p>
 
-    <h3>R3. Verklaar jezelf duidelijk</h3>
-    <p></p>
 	</article>
 </body>
 </html>
